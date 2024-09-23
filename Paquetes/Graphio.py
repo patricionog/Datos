67ce28b15f8c95584ab228f62bef059f33daced2
patricionog <patricioe.nogueroles@gmail.com>
2024-09-22 22:09:03 -0300

    Mejoramiento Total de las Create_Plot

--- Paquetes/Graphio.py
+++ Paquetes/Graphio.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import numpy as np
 
 #######################################################################################################################
 # CREATE #
@@ -11,21 +12,64 @@ import seaborn as sns
 
 # See documentation in "Graphio - Plot Functions.txt" for parameter options.
 
-def Create_Line_Plot(X, Y, Title = None, X_Label = 'X', Y_Label = 'Y', Colors = None, Grid = True, Figure_Size = (10, 6), 
-                     Font_Size = 12, Alpha = 1.0, X_Lim = None, Y_Lim = None, X_Scale = 'linear', Y_Scale = 'linear',
-                     Labels = None, Legend = False, Legend_Location = 'best', Legend_Font_Size = 12, 
-                     Marker_Style = 'o', Line_Style = '-', Line_Width = 2, Horizontal_Lines = None, Vertical_Lines = None, 
-                     Annotations = None, File_Name = None, File_Format = 'png', X_Label_Rotation = 0):
+def Create_Line_Plot(X, Y, Title = None, X_Label = 'X', Y_Label = 'Y', Colors = None, Grid = True, 
+                     Figure_Size = (10, 6), Font_Size = 12, Alpha = 1.0, X_Lim = None, Y_Lim = None, 
+                     X_Scale = 'linear', Y_Scale = 'linear', Labels = None, Legend = False, 
+                     Legend_Location = 'best', Legend_Font_Size = 12, Marker_Styles = 'o', 
+                     Line_Styles = '-', Line_Width = 2, Horizontal_Lines = None, Vertical_Lines = None, 
+                     Annotations = None, File_Name = None, File_Format = 'png', 
+                     X_Label_Rotation = 0, Title_Pad = 20, X_Label_Pad = 10, Y_Label_Pad = 10, 
+                     X_Ticks_Step = None, Y_Ticks_Step = None, X_Ticks = None, Y_Ticks = None):
+    
+    '''
+    Example of use:
+
+    gr.Create_Line_Plot(
+                        X = X_data,
+                        Y = Y_data,
+                        Title = 'Sine and Cosine Waves',
+                        X_Label = 'X-axis (radians)',
+                        Y_Label = 'Y-axis',
+                        Colors = Colors,
+                        Labels = Labels,
+                        Legend = True,
+                        X_Lim = (0, 10),
+                        Y_Lim = (-1.5, 1.5),
+                        Horizontal_Lines = [0],  # Horizontal lines in y=0
+                        Vertical_Lines = [np.pi/2, np.pi],  # Vertical lines in pi/2 and pi
+                        Annotations = [
+                            {'text': 'Max Sine', 'xy': (np.pi/2, 1), 'xytext': (np.pi/2 + 0.5, 1.2), 
+                            'arrowprops': {'facecolor': 'black', 'arrowstyle': '->'}},
+                            {'text': 'Min Cosine', 'xy': (3*np.pi/2, -1), 'xytext': (3*np.pi/2 + 0.5, -1.2), 
+                            'arrowprops': {'facecolor': 'black', 'arrowstyle': '->'}}
+                        ],
+                        File_Name = 'sine_cosine_plot',  
+                        File_Format = 'png',
+                        X_Label_Rotation = 45,
+                        X_Ticks_Step = 1,  
+                        Y_Ticks_Step = 0.5,  
+                        X_Ticks = [2, 4, 6, 8],  # Personalized X_Ticks
+                        Y_Ticks = [-1, 0, 1]     # Personalized Y_Ticks
+                        )
+
+    '''
     
     plt.figure(figsize = Figure_Size)
     
-    for i in range(len(Y)):
-        Color = Colors[i] if Colors else None
-        Label = Labels[i] if Labels and Legend else None
-        
-        plt.plot(X[i], Y[i], color = Color, marker = Marker_Style, linestyle = Line_Style, linewidth = Line_Width, 
-                 alpha = Alpha, label = Label)
-    
+    if isinstance(Y, (list, np.ndarray)):
+        if isinstance(Y, np.ndarray):
+            Y = [Y]
+        for i in range(len(Y)):
+            Color = Colors[i] if Colors and i < len(Colors) else None
+            Label = Labels[i] if Labels and Legend and i < len(Labels) else None
+            Marker_Style = Marker_Styles[i] if isinstance(Marker_Styles, list) and i < len(Marker_Styles) else Marker_Styles
+            Line_Style = Line_Styles[i] if isinstance(Line_Styles, list) and i < len(Line_Styles) else Line_Styles
+            
+            plt.plot(X, Y[i], color = Color, marker = Marker_Style, linestyle = Line_Style, linewidth = Line_Width, 
+                     alpha = Alpha, label = Label)
+    else:
+        raise ValueError("Y must be a list or an array.")
+
     plt.xscale(X_Scale)
     plt.yscale(Y_Scale)
     
@@ -35,26 +79,42 @@ def Create_Line_Plot(X, Y, Title = None, X_Label = 'X', Y_Label = 'Y', Colors = 
         plt.ylim(Y_Lim)
     
     if Title:
-        plt.title(Title, fontsize = Font_Size)
-    plt.xlabel(X_Label, fontsize = Font_Size)
-    plt.ylabel(Y_Label, fontsize = Font_Size)
+        plt.title(Title, fontsize = Font_Size, pad = Title_Pad)
+    plt.xlabel(X_Label, fontsize = Font_Size, labelpad = X_Label_Pad)
+    plt.ylabel(Y_Label, fontsize = Font_Size, labelpad = Y_Label_Pad)
     
     plt.xticks(rotation = X_Label_Rotation)
     
+    Combined_X_Ticks = set()
+    if X_Ticks is not None:
+        Combined_X_Ticks.update(X_Ticks)
+    if X_Ticks_Step is not None and X_Lim is not None:
+        Combined_X_Ticks.update(np.arange(X_Lim[0], X_Lim[1] + 1, step = X_Ticks_Step))
+    
+    plt.xticks(sorted(Combined_X_Ticks))  
+
+    Combined_Y_Ticks = set()
+    if Y_Ticks is not None:
+        Combined_Y_Ticks.update(Y_Ticks)
+    if Y_Ticks_Step is not None and Y_Lim is not None:
+        Combined_Y_Ticks.update(np.arange(Y_Lim[0], Y_Lim[1] + 1, step = Y_Ticks_Step))
+    
+    plt.yticks(sorted(Combined_Y_Ticks))  
+
     if Grid:
         plt.grid(True)
     
     if Horizontal_Lines:
-        for line in Horizontal_Lines:
-            plt.axhline(y = line, color = 'gray', linestyle = '--')
+        for Line in Horizontal_Lines:
+            plt.axhline(y = Line, color = 'gray', linestyle = '--')
     if Vertical_Lines:
-        for line in Vertical_Lines:
-            plt.axvline(x = line, color = 'gray', linestyle = '--')
+        for Line in Vertical_Lines:
+            plt.axvline(x = Line, color = 'gray', linestyle = '--')
     
     if Annotations:
-        for annotation in Annotations:
-            plt.annotate(annotation['text'], xy = annotation['xy'], xytext = annotation['xytext'], 
-                         arrowprops = annotation.get('arrowprops', {}))
+        for Annotation in Annotations:
+            plt.annotate(Annotation['text'], xy = Annotation['xy'], xytext = Annotation['xytext'], 
+                         arrowprops = Annotation.get('arrowprops', {}))
     
     if Legend and Labels:
         plt.legend(loc = Legend_Location, fontsize = Legend_Font_Size)
@@ -109,38 +169,50 @@ def Create_Histogram(Data, Title = None, X_Label = 'X', Y_Label = 'Y', Bins = 10
     
     plt.show()
 
-def Create_Bar_Plot(Categories, Values, Title = None, X_Label = 'X', Y_Label = 'Y', Colors = None, Grid = True, Figure_Size = (10, 6), 
-                    Font_Size = 12, Orientation = 'vertical', X_Lim = None, Y_Lim = None, Legend = False, 
-                    Legend_Location = 'best', Legend_Font_Size = 12, File_Name = None, File_Format = 'png'):
+def Create_Bar_Plot(X, Y, Title = None, X_Label = 'X', Y_Label = 'Y', Colors = None, Grid = True, Figure_Size = (10, 6), 
+                     Font_Size = 12, Alpha = 1.0, X_Lim = None, Y_Lim = None, 
+                     X_Ticks = None, Y_Ticks = None, X_Ticks_Step = None, Y_Ticks_Step = None, 
+                     Horizontal_Lines = None, Vertical_Lines = None, 
+                     Annotations = None, File_Name = None, File_Format = 'png'):
     
     plt.figure(figsize = Figure_Size)
-    
-    if Colors:
-        if Orientation == 'horizontal':
-            plt.barh(Categories, Values, color = Colors)
-        else:
-            plt.bar(Categories, Values, color = Colors)
-    else:
-        if Orientation == 'horizontal':
-            plt.barh(Categories, Values)
-        else:
-            plt.bar(Categories, Values)
-    
-    if X_Lim:
-        plt.xlim(X_Lim)
-    if Y_Lim:
-        plt.ylim(Y_Lim)
+    plt.bar(X, Y, color = Colors, alpha = Alpha)
     
     if Title:
         plt.title(Title, fontsize = Font_Size)
     plt.xlabel(X_Label, fontsize = Font_Size)
     plt.ylabel(Y_Label, fontsize = Font_Size)
     
-    if Grid:
-        plt.grid(axis = 'y')
+    Combined_X_Ticks = set()
+    if X_Ticks is not None:
+        Combined_X_Ticks.update(X_Ticks)
+    if X_Ticks_Step is not None:
+        Combined_X_Ticks.update(np.arange(X_Lim[0], X_Lim[1] + 1, step=X_Ticks_Step))
     
-    if Legend:
-        plt.legend(loc = Legend_Location, fontsize = Legend_Font_Size)
+    plt.xticks(sorted(Combined_X_Ticks))
+    
+    Combined_Y_Ticks = set()
+    if Y_Ticks is not None:
+        Combined_Y_Ticks.update(Y_Ticks)
+    if Y_Ticks_Step is not None:
+        Combined_Y_Ticks.update(np.arange(Y_Lim[0], Y_Lim[1] + 1, step=Y_Ticks_Step))
+    
+    plt.yticks(sorted(Combined_Y_Ticks))
+
+    if Grid:
+        plt.grid(True)
+    
+    if Horizontal_Lines:
+        for line in Horizontal_Lines:
+            plt.axhline(y = line, color = 'gray', linestyle = '--')
+    if Vertical_Lines:
+        for line in Vertical_Lines:
+            plt.axvline(x = line, color = 'gray', linestyle = '--')
+    
+    if Annotations:
+        for annotation in Annotations:
+            plt.annotate(annotation['text'], xy = annotation['xy'], xytext = annotation['xytext'], 
+                         arrowprops = annotation.get('arrowprops', {}))
     
     if File_Name:
         plt.savefig(f'{File_Name}.{File_Format}', format = File_Format, bbox_inches = 'tight')
@@ -175,41 +247,39 @@ def Create_Box_Plot(Data, Title = None, X_Label = 'X', Y_Label = 'Y', Grid = Tru
     
     plt.show()
 
-def Create_Scatter_Plot_With_Regression(X, Y, Title = None, X_Label = 'X', Y_Label = 'Y', Colors = None, Grid = True, 
-                                         Figure_Size = (10, 6), Font_Size = 12, Alpha = 1.0, X_Lim = None, Y_Lim = None, 
-                                         X_Scale = 'linear', Y_Scale = 'linear', Legend = False, Legend_Location = 'best', 
-                                         Legend_Font_Size = 12, Horizontal_Lines = None, Vertical_Lines = None, 
-                                         Annotations = None, File_Name = None, File_Format = 'png'):
+def Create_Scatter_Plot(X, Y, Title = None, X_Label = 'X', Y_Label = 'Y', Colors = None, Grid = True, Figure_Size = (10, 6), 
+                         Font_Size = 12, Alpha = 1.0, X_Lim = None, Y_Lim = None, 
+                         X_Ticks = None, Y_Ticks = None, X_Ticks_Step = None, Y_Ticks_Step = None, 
+                         Annotations = None, File_Name = None, File_Format = 'png', 
+                         Legend = False, Legend_Location = 'best', Legend_Font_Size = 12):
     
     plt.figure(figsize = Figure_Size)
     
-    if Colors:
-        sns.regplot(x = X, y = Y, scatter_kws = {'color': Colors[0], 'alpha': Alpha}, line_kws = {'color': 'red'})
-    else:
-        sns.regplot(x = X, y = Y, scatter_kws = {'alpha': Alpha}, line_kws = {'color': 'red'})
-    
-    plt.xscale(X_Scale)
-    plt.yscale(Y_Scale)
-    
-    if X_Lim:
-        plt.xlim(X_Lim)
-    if Y_Lim:
-        plt.ylim(Y_Lim)
+    plt.scatter(X, Y, color = Colors, alpha = Alpha)
     
     if Title:
         plt.title(Title, fontsize = Font_Size)
     plt.xlabel(X_Label, fontsize = Font_Size)
     plt.ylabel(Y_Label, fontsize = Font_Size)
+
+    Combined_X_Ticks = set()
+    if X_Ticks is not None:
+        Combined_X_Ticks.update(X_Ticks)
+    if X_Ticks_Step is not None and X_Lim is not None:
+        Combined_X_Ticks.update(np.arange(X_Lim[0], X_Lim[1] + 1, step = X_Ticks_Step))
     
+    plt.xticks(sorted(Combined_X_Ticks))
+    
+    Combined_Y_Ticks = set()
+    if Y_Ticks is not None:
+        Combined_Y_Ticks.update(Y_Ticks)
+    if Y_Ticks_Step is not None and Y_Lim is not None:
+        Combined_Y_Ticks.update(np.arange(Y_Lim[0], Y_Lim[1] + 1, step = Y_Ticks_Step))
+    
+    plt.yticks(sorted(Combined_Y_Ticks))
+
     if Grid:
         plt.grid(True)
-    
-    if Horizontal_Lines:
-        for line in Horizontal_Lines:
-            plt.axhline(y = line, color = 'gray', linestyle = '--')
-    if Vertical_Lines:
-        for line in Vertical_Lines:
-            plt.axvline(x = line, color = 'gray', linestyle = '--')
     
     if Annotations:
         for annotation in Annotations:
@@ -224,27 +294,50 @@ def Create_Scatter_Plot_With_Regression(X, Y, Title = None, X_Label = 'X', Y_Lab
     
     plt.show()
 
-def Create_Violin_Plot(Data, Title = None, X_Label = 'X', Y_Label = 'Y', Grid = True, Figure_Size = (10, 6), 
-                       Font_Size = 12, X_Lim = None, Y_Lim = None, Label = None, Legend = False):
+def Create_Violin_Plot(Data, Title = None, X_Label = 'X', Y_Label = 'Y', Colors = None, Grid = True, Figure_Size = (10, 6), 
+                        Font_Size = 12, Alpha = 1.0, X_Ticks = None, Y_Ticks = None, 
+                        X_Ticks_Step = None, Y_Ticks_Step = None, 
+                        Annotations = None, File_Name = None, File_Format = 'png', 
+                        Legend = False, Legend_Location = 'best', Legend_Font_Size = 12):
     
-    plt.figure(figsize=Figure_Size)
-    sns.violinplot(data=Data)
+    plt.figure(figsize = Figure_Size)
     
-    if X_Lim:
-        plt.xlim(X_Lim)
-    if Y_Lim:
-        plt.ylim(Y_Lim)
+    plt.violinplot(Data, showmeans=True, showmedians=True)
     
     if Title:
-        plt.title(Title, fontsize=Font_Size)
-    plt.xlabel(X_Label, fontsize=Font_Size)
-    plt.ylabel(Y_Label, fontsize=Font_Size)
+        plt.title(Title, fontsize = Font_Size)
+    plt.xlabel(X_Label, fontsize = Font_Size)
+    plt.ylabel(Y_Label, fontsize = Font_Size)
     
+    Combined_X_Ticks = set()
+    if X_Ticks is not None:
+        Combined_X_Ticks.update(X_Ticks)
+    if X_Ticks_Step is not None:
+        Combined_X_Ticks.update(np.arange(1, len(Data) + 1, step = X_Ticks_Step))
+    
+    plt.xticks(sorted(Combined_X_Ticks))
+    
+    Combined_Y_Ticks = set()
+    if Y_Ticks is not None:
+        Combined_Y_Ticks.update(Y_Ticks)
+    if Y_Ticks_Step is not None:
+        Combined_Y_Ticks.update(np.arange(min(np.concatenate(Data)), max(np.concatenate(Data)) + 1, step = Y_Ticks_Step))
+    
+    plt.yticks(sorted(Combined_Y_Ticks))
+
     if Grid:
         plt.grid(True)
     
-    if Legend and Label:
-        plt.legend()
+    if Annotations:
+        for annotation in Annotations:
+            plt.annotate(annotation['text'], xy = annotation['xy'], xytext = annotation['xytext'], 
+                         arrowprops = annotation.get('arrowprops', {}))
+    
+    if Legend:
+        plt.legend(loc = Legend_Location, fontsize = Legend_Font_Size)
+    
+    if File_Name:
+        plt.savefig(f'{File_Name}.{File_Format}', format = File_Format, bbox_inches = 'tight')
     
     plt.show()
 
